f68cb0360e558320a2a5f5ea667c08a013406731
Sofia4041 <353124127@qq.com>
2019-03-18 21:41:07 +0800

    change

--- sample/src/main/java/com/danikula/videocache/sample/Video.java
+++ sample/src/main/java/com/danikula/videocache/sample/Video.java
@@ -2,11 +2,11 @@ package com.danikula.videocache.sample;
 
 public enum Video {
 
-    ORANGE_1(Config.ROOT + "orange1.mp4"),
-    ORANGE_2(Config.ROOT + "orange2.mp4"),
-    ORANGE_3(Config.ROOT + "orange3.mp4"),
-    ORANGE_4(Config.ROOT + "orange4.mp4"),
-    ORANGE_5(Config.ROOT + "orange5.mp4");
+    ORANGE_1("http://dev-static01-joker.taihe.com/0208/M00/54/20/ChR461yHc9GAYIVUAzrAnan0gxU368.mp4"),
+    ORANGE_2("http://dev-static01-joker.taihe.com/0207/M00/54/93/ChR47FyHdFqAWLCQAvMZDg22lpc164.mp4"),
+    ORANGE_3("http://dev-static01-joker.taihe.com/0209/M00/59/2E/ChR461yHctiARbGQAzO4WcVQV4o061.mp4"),
+    ORANGE_4("http://dev-static01-joker.taihe.com/0207/M00/54/95/ChR47FyKCTiAawQ1ABkVoXmFp70269.mp4?_width=960&_height=540"),
+    ORANGE_5("http://dev-static01-joker.taihe.com/0207/M00/54/83/ChR461x3SiKAAEhYAB4vGRb4MW8276.mp4?_width=854&_height=480");
 
     public final String url;
 
